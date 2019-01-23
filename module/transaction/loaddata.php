f92a5ca0b2db787b7a2cb1b979585801d6c79bf1
Arnaud GUY <contact@arnaudguy.fr>
2019-01-23 22:51:45 +0100

    clean file

--- module/transaction/loaddata.php
+++ module/transaction/loaddata.php
@@ -1,20 +1,4 @@
 <?php
-
-/*
- * examples/mysql/loaddata.php
- *
- * This file is part of EditableGrid.
- * http://editablegrid.net
- *
- * Copyright (c) 2011 Webismymind SPRL
- * Dual licensed under the MIT or GPL Version 2 licenses.
- * http://editablegrid.net/license
- */
-
-/**
- * This script loads data from the database and returns it to the js
- *
- */
 include '../../global/config.php';
 include '../../model/EditableGrid.php';
 require_once '../../lib/pdo2.php';
@@ -22,10 +6,6 @@ require_once '../../lib/pdo2.php';
 $pdo = PDO2::getInstance();
 $pdo->exec("set names utf8");
 
-/**
- * fetch_pairs is a simple method that transforms a mysqli_result object in an array.
- * It will be used to generate possible values for some columns.
- */
 function fetch_pairs($pdo, $query)
 {
     if (!($res = $pdo->query($query))) {
@@ -48,15 +28,10 @@ function fetch_pairs($pdo, $query)
 
 $grid = new EditableGrid();
 
-/*
- *  Add columns. The first argument of addColumn is the name of the field in the databse.
- *  The second argument is the label that will be displayed in the header
- */
 //$grid->addColumn('id', 'ID', 'integer', null, false);
 $grid->addColumn('date2', 'Date', 'date');
 $grid->addColumn('id_type', 'Type', 'string', fetch_pairs($pdo, 'SELECT id, name FROM type'), true);
-$grid->addColumn('id_category', 'Catégorie', 'string', fetch_pairs($pdo, 'SELECT id, name FROM category WHERE id_base = '.$_SESSION['activebase'].''), true);
-//$grid->addColumn('id_sscategory', 'Sous Catégorie', 'string', fetch_pairs($pdo, 'SELECT id, name FROM sscategory'), true);
+$grid->addColumn('id_category', 'Catégorie', 'string', fetch_pairs($pdo, 'SELECT id, name FROM category WHERE id_base = ' . $_SESSION['activebase'] . ''), true);
 $grid->addColumn('third', 'Tiers', 'string');
 $grid->addColumn('comment', 'Commentaire', 'string');
 $grid->addColumn('amount', 'Montant', 'float');
@@ -64,9 +39,9 @@ $grid->addColumn('tally', 'Pointage', 'boolean');
 $base = $_SESSION['activebase'];
 $sql = "SELECT id, name FROM bank WHERE id_base = $base AND system = 0";
 $grid->addColumn('id_bank', 'Banque', 'string', fetch_pairs($pdo, $sql), true);
-if ($_SESSION['activecontrats']== 1) {
-    $grid->addColumn('id_contrat', 'Contrats', 'string',fetch_pairs($pdo, 'SELECT id, name FROM contrats WHERE id_base = '.$_SESSION['activebase'].''), true);
-
+if ($_SESSION['activecontrats'] == 1) 
+{
+    $grid->addColumn('id_contrat', 'Contrats', 'string', fetch_pairs($pdo, 'SELECT id, name FROM contrats WHERE id_base = ' . $_SESSION['activebase'] . ''), true);
 }
 $grid->addColumn('action', 'Action', 'html', null, false, 'id');
 
@@ -80,28 +55,25 @@ $queryCount = "SELECT count(id) as nb FROM $mydb_tablename WHERE id_base = $base
 $totalUnfiltered = $pdo->query($queryCount)->fetch()[0];
 $total = $totalUnfiltered;
 
-/* SERVER SIDE */
-/* If you have set serverSide : true in your Javascript code, $_GET contains 3 additionnal parameters : page, filter, sort
- * this parameters allow you to adapt your query
- *
- */
 $page = 0;
-if (isset($_GET['page']) && is_numeric($_GET['page'])) {
+if (isset($_GET['page']) && is_numeric($_GET['page'])) 
+{
     $page = (int) $_GET['page'];
 }
 
-$rowByPage = 50;
-
+$rowByPage = 20;
 $from = ($page - 1) * $rowByPage;
 
-if (isset($_GET['filter']) && $_GET['filter'] != "") {
+if (isset($_GET['filter']) && $_GET['filter'] != "") 
+{
     $filter = $_GET['filter'];
     $query .= '  AND third like "%' . $filter . '%"';
     $queryCount .= '  AND third like "%' . $filter . '%"';
     $total = $pdo->query($queryCount)->fetch()[0];
 }
 if ($_GET['sort'] == "date2") {$_GET['sort'] = "date";}
-if (isset($_GET['sort']) && $_GET['sort'] != "") {
+if (isset($_GET['sort']) && $_GET['sort'] != "") 
+{
     $query .= " ORDER BY " . $_GET['sort'] . ($_GET['asc'] == "0" ? " DESC " : "");
 }
 
@@ -112,16 +84,11 @@ error_log("total = " . $total);
 error_log("totalUnfiltered = " . $totalUnfiltered);
 
 $grid->setPaginator(ceil($total / $rowByPage), (int) $total, (int) $totalUnfiltered, null);
-
-/* END SERVER SIDE */
-
 error_log($query);
-//echo $query
 $result = $pdo->query($query);
 
 // close PDO
 $pdo = null;
 
-// send data to the browser
-
+// envoie data
 $grid->renderJSON($result, false, false, !isset($_GET['data_only']));
