c00426345f48e0b6984f4084f4183f3f9d1fe5ea
Arnaud GUY <contact@arnaudguy.fr>
2019-01-30 19:16:06 +0100

    facture add js

--- module/facture/list.php
+++ module/facture/list.php
@@ -1,6 +1,6 @@
 <?php ob_start();?>
 
-<script src="public/js/bank.js"></script>
+<script src="public/js/facture.js"></script>
 
 <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
   <div class="card">
